11e249e5caa55927a523bddb792a3303551cedfd
Zapharaos <matthieu.freitag@gmail.com>
2024-07-11 13:24:23 +0200

    feat: update api: user test utils

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,154 +1,74 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from user.models import (
-    File, Technology, Project, Experience, WorkItem, Work, Hero, About, Footer, User, Social
-)
-from user.tests.utils import create_sample_user
+from user.models import User
+from user.tests.utils import create_sample_user, create_sample_file, create_sample_technology, create_sample_project, \
+    create_sample_experience, create_sample_work_item, create_sample_work, create_sample_hero, create_sample_about, \
+    create_sample_footer, create_sample_social
 
 
 class FileModelTest(TestCase):
     def test_create_file(self):
-        file = File.objects.create(name="Test File", file="path/to/file.txt")
-        self.assertEqual(file.name, "Test File")
+        file = create_sample_file(name="Name")
+        self.assertEqual(file.name, "Name")
 
 
 class TechnologyModelTest(TestCase):
     def test_create_technology(self):
-        technology = Technology.objects.create(name="Python")
-        self.assertEqual(technology.name, "Python")
+        technology = create_sample_technology(name="Name")
+        self.assertEqual(technology.name, "Name")
 
 
 class ProjectModelTest(TestCase):
     def test_create_project(self):
-        file = File.objects.create(name="Project Image", file="path/to/image.jpg")
-        technology = Technology.objects.create(name="Django")
-        project = Project.objects.create(
-            index=1,
-            hidden=False,
-            url="https://project.com",
-            title="Project Title",
-            description="Project Description",
-            image=file,
-        )
-        project.technologies.set([technology])
-        self.assertEqual(project.title, "Project Title")
-        self.assertEqual(project.technologies.count(), 1)
+        project = create_sample_project(title="Title")
+        self.assertEqual(project.title, "Title")
 
 
 class ExperienceModelTest(TestCase):
     def test_create_experience(self):
-        experience = Experience.objects.create(
-            index=1,
-            hidden=False,
-            title="Experience Title",
-            organisation="Some Organization",
-            period="2020-2022",
-            description="Experience Description",
-        )
-        self.assertEqual(experience.title, "Experience Title")
+        experience = create_sample_experience(title="Title")
+        self.assertEqual(experience.title, "Title")
 
     def test_url_short_validation(self):
         with self.assertRaises(ValidationError):
-            experience = Experience(
-                urlShort="https://short.com",
-            )
+            experience = create_sample_experience(title="Title", url=None)
             experience.clean()
-
-    def test_valid_url_short(self):
-        experience = Experience.objects.create(
-            index=1,
-            hidden=False,
-            title="Experience Title",
-            organisation="Some Organization",
-            period="2020-2022",
-            description="Experience Description",
-            url="https://experience.com",
-            urlShort="https://short.com",
-        )
-        self.assertEqual(experience.urlShort, "https://short.com")
 
 
 class WorkItemModelTest(TestCase):
     def test_create_work_item(self):
-        file = File.objects.create(name="Project Image", file="path/to/image.jpg")
-        project = Project.objects.create(
-            index=1,
-            hidden=False,
-            url="https://project.com",
-            title="Project Title",
-            description="Project Description",
-            image=file,
-        )
-        experience = Experience.objects.create(
-            index=1,
-            hidden=False,
-            title="Experience Title",
-            organisation="Some Organization",
-            period="2020-2022",
-            description="Experience Description",
-        )
-        work_item = WorkItem.objects.create(
-            index=1,
-            hidden=False,
-            title="Work Item Title",
-            showProjects=True,
-        )
-        work_item.projects.set([project])
-        work_item.experiences.set([experience])
-        self.assertEqual(work_item.title, "Work Item Title")
-        self.assertEqual(work_item.projects.count(), 1)
-        self.assertEqual(work_item.experiences.count(), 1)
-        self.assertTrue(work_item.showProjects)
-        self.assertFalse(work_item.showExperiences)
+        work_item = create_sample_work_item(title="Title")
+        self.assertEqual(work_item.title, "Title")
 
     def test_validation_show_fields(self):
         with self.assertRaises(ValidationError):
-            work_item = WorkItem(
-                index=1,
-                hidden=False,
-                title="Work Item Title",
-                showProjects=True,
-                showExperiences=True,
-            )
+            work_item = create_sample_work_item(title="Title", showProjects=True, showExperiences=True)
             work_item.clean()
 
 
 class WorkModelTest(TestCase):
 
     def test_create_work(self):
-        work = Work.objects.create(content_type="work")
-        self.assertEqual(work.content_type, "work")
+        work = create_sample_work(content_type="Work")
+        self.assertEqual(work.content_type, "Work")
 
 
 class HeroModelTest(TestCase):
     def test_create_hero(self):
-        file = File.objects.create(name="Background Image", file="path/to/image.jpg")
-        hero = Hero.objects.create(
-            title="Hero Title",
-            tagline="Hero Tagline",
-            callToActionContent="Call to Action Text",
-            backgroundImage=file,
-        )
-        self.assertEqual(hero.title, "Hero Title")
+        hero = create_sample_hero(content_type="Hero")
+        self.assertEqual(hero.content_type, "Hero")
 
 
 class AboutModelTest(TestCase):
     def test_create_about(self):
-        file = File.objects.create(name="About Image", file="path/to/image.jpg")
-        about = About.objects.create(
-            description="About Description",
-            image=file,
-        )
-        self.assertEqual(about.description, "About Description")
+        about = create_sample_about(content_type="About")
+        self.assertEqual(about.content_type, "About")
 
 
 class FooterModelTest(TestCase):
     def test_create_footer(self):
-        footer = Footer.objects.create(
-            title="Footer Title",
-            subTitle="Footer Subtitle",
-        )
-        self.assertEqual(footer.title, "Footer Title")
+        footer = create_sample_footer(content_type="Footer")
+        self.assertEqual(footer.content_type, "Footer")
 
 
 class UserModelTest(TestCase):
@@ -165,13 +85,5 @@ class UserModelTest(TestCase):
 
 class SocialModelTest(TestCase):
     def test_create_social(self):
-        user = create_sample_user()
-        file = File.objects.create(name="Social Icon", file="path/to/icon.jpg")
-        social = Social.objects.create(
-            idUser=user,
-            index=1,
-            name="Social Network",
-            url="https://social.com",
-            image=file,
-        )
-        self.assertEqual(social.name, "Social Network")
+        social = create_sample_social(name="Name")
+        self.assertEqual(social.name, "Name")
--- user/tests/utils.py
+++ user/tests/utils.py
@@ -1,25 +1,79 @@
-from user.models import (
-    File, Work, Hero, About, Footer, User
-)
+from user.models import File, Technology, Project, Experience, WorkItem, Work, Hero, About, Footer, User, Social
+
+
+def create_sample_file(name="Name"):
+    return File.objects.create(name=name, file="path/to/logo.png")
+
+
+def create_sample_technology(name="Name"):
+    return Technology.objects.create(name=name)
+
+
+def create_sample_project(title="Title"):
+    project = Project.objects.create(
+        index=1, hidden=False, url="https://project.com", title=title,
+        description="A short description", image=create_sample_file()
+    )
+    project.technologies.set([create_sample_technology()])
+    return project
+
+
+def create_sample_experience(title="Title", url="https://experience.com"):
+    experience = Experience.objects.create(
+        index=1, hidden=False, title=title, organisation="Some Organization", period="2020-2022",
+        location="fr-FR", url=url, urlShort="https://short.com", description="Description",
+    )
+    experience.technologies.set([create_sample_technology()])
+    return experience
+
+
+def create_sample_work_item(title="Title", showProjects=True, showExperiences=False):
+    item = WorkItem.objects.create(
+        index=1, hidden=False, title=title, showProjects=showProjects, showExperiences=showExperiences
+    )
+    item.projects.set([create_sample_project()])
+    item.experiences.set([create_sample_experience()])
+    return item
+
+
+def create_sample_work(content_type="content_type"):
+    work = Work.objects.create(content_type=content_type)
+    work.items.set([create_sample_work_item()])
+    return work
+
+
+def create_sample_hero(content_type="content_type"):
+    return Hero.objects.create(
+        content_type=content_type, title="Hero", tagline="Tagline",
+        callToActionContent="Click", backgroundImage=create_sample_file()
+    )
+
+
+def create_sample_about(content_type="content_type"):
+    return About.objects.create(
+        content_type=content_type, image=create_sample_file(),
+        imageResponsive=create_sample_file(), description="Description"
+    )
+
+
+def create_sample_footer(content_type="content_type"):
+    return Footer.objects.create(
+        content_type=content_type, title="Sample Footer Title", subTitle="Sample Footer Subtitle",
+        showLocation=True, showSocials=True, showEmail=True, showResume=True
+    )
 
 
 def create_sample_user():
-    """
-    Creates a sample user with pre-defined details and related objects.
+    return User.objects.create(
+        name="Test User", email="test@example.com", location="location", locale="locale",
+        logo=create_sample_file(), resume=create_sample_file(),
+        hero=create_sample_hero(), about=create_sample_about(),
+        work=create_sample_work(), footer=create_sample_footer()
+    )
 
-    Returns:
-        The newly created User object with related objects.
-    """
 
-    file = File.objects.create(name="Logo", file="path/to/logo.png")
-    resume = File.objects.create(name="Resume", file="path/to/resume.pdf")
-    hero = Hero.objects.create(title="Hero Title", tagline="Hero Tagline", callToActionContent="Click Here",
-                               backgroundImage=file)
-    about = About.objects.create(image=file, description="About description")
-    work = Work.objects.create(content_type="work")
-    footer = Footer.objects.create(title="Footer Title", subTitle="Footer SubTitle")
-
-    user = User.objects.create(name="Test User", email="test@example.com", logo=file, resume=resume, hero=hero,
-                               about=about, work=work, footer=footer)
-
-    return user
+def create_sample_social(name="Name"):
+    return Social.objects.create(
+        idUser=create_sample_user(), index=1, hidden=False,
+        name=name, pseudo="Pseudo", url="https://social.com", image=create_sample_file()
+    )
